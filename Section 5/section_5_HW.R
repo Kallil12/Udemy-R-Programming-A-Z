3d46df6290c89cabcf799871ce3acbbf5f359eb6
Kallil12 <kallilaraujo@gmail.com>
2019-06-10 09:43:41 -0300

    Data Frames

--- Section 5/section_5_HW.R
+++ Section 5/section_5_HW.R
@@ -12,7 +12,7 @@ data_1960 <- countries_hw[countries_hw$Year == 1960,] # the result is a data fra
 data_2013 <- countries_hw[countries_hw$Year == 2013,]
 
 # Now we need to merge the data frames created above using the country Code
-# merged <- merge(stats, mydf, by.x = "Country.Code", by.y = "Code")
+
 head(data_1960)
 head(life_expect_1960)
 countries_1960 <- merge(data_1960, life_expect_1960, by.x="Country.Code",by.y="Code")
@@ -25,6 +25,6 @@ countries_2013 <- merge(data_2013, life_expect_2013, by.x="Country.Code",by.y="C
 
 head(countries_2013)
 # With all the data in place, we can now use qplot to make the graphics
-# qplot(data=stats, x=Income.Group, y=Birth.rate, size=I(3))
+
 qplot(data=countries_1960, x=Fertility.Rate, y=Life_1960,color=Region,alpha=I(0.6))
 qplot(data=countries_2013, x=Fertility.Rate, y=Life_2013,color=Region,alpha=I(0.6))
